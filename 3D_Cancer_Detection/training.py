fd8e05e536fb18b0715a50f850b002542738c7ed
Andreas Michael <egads1@MacBook-Pro.lan>
2022-10-06 15:46:08 -0300

    Initialized training script

---
+++ 3D_Cancer_Detection/training.py
@@ -0,0 +1,58 @@
+import argparse
+import datetime
+import os
+import sys
+
+import numpy as np
+
+from torch.utils.tensorboard import SummaryWriter
+
+import torch
+import torch.nn as nn
+from torch.optim import SGD, Adam
+from torch.utils.data import DataLoader
+
+from util.util import enumerateWithEstimate
+from dsets import LunaDataset
+from util.logconf import logging
+from model import LunaModel
+
+log = logging.getLogger(__name__)
+# log.setLevel(logging.WARN)
+log.setLevel(logging.INFO)
+log.setLevel(logging.DEBUG)
+
+# Used for computeBatchLoss and logMetrics to index into metrics_t/metrics_a
+METRICS_LABEL_NDX=0
+METRICS_PRED_NDX=1
+METRICS_LOSS_NDX=2
+METRICS_SIZE = 3
+
+class LunaTrainingApp:
+    def __init__(self, sys_argv=None):
+        if sys_argv is None:
+            sys_argv = sys.argv[1:]
+
+        parser = argparse.ArgumentParser()
+        parser.add_argument('--num-workers',
+            help='Number of worker processes for background data loading',
+            default=8,
+            type=int,
+        )
+        parser.add_argument('--epochs',
+            help='Number of epochs to train for',
+            default=1,
+            type=int,
+        )
+
+        self.cli_args = parser.parse_args(sys_argv)
+        self.time_str = datetime.datetime.now().strftime('%Y-%m-%d_%H.%M.%S')
+
+
+    def main(self):
+        log.info("Starting {}, {}".format(type(self).__name__, self.cli_args))
+
+    
+
+if __name__ == '__main__':
+    LunaTrainingApp().main()
